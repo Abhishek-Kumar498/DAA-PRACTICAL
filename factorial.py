9795e4cc893623b3a6dae13c928840c206e6e002
Abhishek-kumar <abhishek.kumar915531@gmail.com>
2026-08-14 15:51:11 +0530

    Add iterative factorial function in factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+n = int(input ("enter a number "))
+print("factorial is ",factorial_iterative(n)) 
